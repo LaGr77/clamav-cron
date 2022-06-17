abd8ed89dd0fb86a1efb8c51db16509c99ba8703
Laďa <67732401+LaGr77@users.noreply.github.com>
2022-06-17 10:54:05 +0200

    CHANGE: Write 2 log X Store to variable

--- clamdaily.sh
+++ clamdaily.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # written by Ladislav Grulich (LaGr77)
-# version 1.0.1
-# since 2022-01-26
+# version 1.1.0
+# since 2022-06-17
 # licence MIT
 
 CLAM_LOGFILE="/var/log/clamav/clamav-$(date +'%w').log"
@@ -10,11 +10,13 @@ CLAM_EMAIL_CC="email-2@gmail.com"
 CLAM_EMAIL_FROM="ClamAV@$HOSTNAME"
 
 if [ "$(date +'%w')" == 1 ]; then
-	nice -n0 clamscan --recursive --infected / --exclude-dir=/proc/ --exclude-dir=/sys/ &>"${CLAM_LOGFILE}";
+	#nice -n0 clamscan --recursive --infected / --exclude-dir=/proc/ --exclude-dir=/sys/ &>"${CLAM_LOGFILE}";
+	CLAM_MSG=$(nice -n10 clamscan --recursive --infected / --exclude-dir=/proc/ --exclude-dir=/sys/)
 	#--log="${CLAM_LOGFILE}"
 	CLAM_SUBJ="FULL"
 else
-	nice -n0 clamscan --recursive --infected /home &>"${CLAM_LOGFILE}";
+	#nice -n0 clamscan --recursive --infected /home &>"${CLAM_LOGFILE}";
+	CLAM_MSG=$(nice -n10 clamscan --recursive --infected /home)
 	#--log="${CLAM_LOGFILE}"
 	CLAM_SUBJ="HOME"
 fi
@@ -25,7 +27,8 @@ CLAM_INFECTED=$(cat "${CLAM_LOGFILE}" | grep Infected);
 	clamscan --version
 	echo ""
 	echo "---------- INFECTED FILES ----------"
-	cat "${CLAM_LOGFILE}"
+	#cat "${CLAM_LOGFILE}"
+	echo $CLAM_MSG
 ) | mail -s "[CLAMAV:${CLAM_SUBJ}] ${HOSTNAME}: ${CLAM_INFECTED}" -r "${CLAM_EMAIL_FROM}" "${CLAM_EMAIL}","${CLAM_EMAIL_CC}"
 
 
